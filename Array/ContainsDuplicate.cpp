b8f286c2cea70ceadaf6a42e187a262a30aa4d21
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-06-19 15:04:26 -0700

    Create ContainsDuplicate.cpp
    Runtime: 90 ms
    Memory Usage: 51.2 MB

---
+++ Array/ContainsDuplicate.cpp
@@ -0,0 +1,23 @@
+/*
+Problem: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+https://leetcode.com/problems/contains-duplicate/
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        if (nums.size() < 2)
+            return false;
+        
+        unordered_set<int> uset; //Space : O(N)
+        for (auto itr = nums.begin(); itr < nums.end(); itr++) { //Time: O(N)
+            auto temp = uset.find(*itr);
+            if (temp != uset.end()) 
+                return true;
+            
+            uset.insert(*itr);
+        }
+        return false;
+    }
+};
