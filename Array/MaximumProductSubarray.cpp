b6d518444581a84e4614e03618ab75e259af8a8d
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-06-19 23:01:44 -0700

    Create MaximumProductSubarray.cpp
    Runtime: 4 ms
    Memory Usage: 13.8 MB

---
+++ Array/MaximumProductSubarray.cpp
@@ -0,0 +1,35 @@
+/*
+Problem : Given an integer array nums, find a contiguous non-empty subarray within the array that has the largest product, and return the product.
+The test cases are generated so that the answer will fit in a 32-bit integer.
+A subarray is a contiguous subsequence of the array.
+
+https://leetcode.com/problems/maximum-product-subarray/
+*/
+
+#include <limits.h>
+#include <algorithm>
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.size() < 2) {
+            return (nums.front());
+        }
+        
+        int res = INT_MIN;
+        int max_P = 1;
+        int min_P = 1;
+        
+        for (auto itr = nums.begin(); itr < nums.end(); itr++) { //Time: O(N)
+            if (*itr < 0) {
+                swap(max_P, min_P);
+            }
+            //Storing max product of pos numbers
+            max_P = max(max_P*(*itr), *itr);
+            
+            //Storing max product of neg numbers
+            min_P = min(min_P*(*itr), *itr);
+            res = max(res, max_P);
+        }
+        return res;
+    }
+};
