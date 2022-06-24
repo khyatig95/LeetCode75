ae9099d094aaf0dcbd9c52572a29e28de8b049c1
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-06-24 01:44:13 -0700

    Update ContainsDuplicate.cpp

--- Array/ContainsDuplicate.cpp
+++ Array/ContainsDuplicate.cpp
@@ -2,6 +2,9 @@
 Problem: Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
 https://leetcode.com/problems/contains-duplicate/
+
+Time Complexity : O(N)
+Space Complexity : O(N)
 */
 
 class Solution {
