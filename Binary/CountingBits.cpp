779c14c13225f2672f8b9e45beff7af1af346a04
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-06-25 01:03:48 -0700

    Update CountingBits.cpp

--- Binary/CountingBits.cpp
+++ Binary/CountingBits.cpp
@@ -1,5 +1,7 @@
 /*
 Problem : Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+Follow up:
+It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
 
 https://leetcode.com/problems/counting-bits/
 
